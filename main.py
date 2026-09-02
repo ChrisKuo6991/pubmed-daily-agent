218ec882254843af388ffd35069b9a4de420648d
Chris <86276974+ChrisKuo6991@users.noreply.github.com>
2026-09-02 16:10:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import requests
 SEARCH_KEYWORDS = ["Microbiom", "metagenome", "metagenomic"]
 SEARCH_TERM = " OR ".join(SEARCH_KEYWORDS)
 
-MAX_RESULTS = 20
+MAX_RESULTS = 30
 EXCEL_IF_PATH = "JCR-ImapctFactor-2025.xlsx"
 DB_EXCEL_PATH = "papers_database.xlsx"
 
