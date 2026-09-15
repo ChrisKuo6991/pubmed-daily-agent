e45179d438b5c3d6a9e031cdb9e41428775727b9
Chris <86276974+ChrisKuo6991@users.noreply.github.com>
2026-09-15 18:41:35 +0800

    Update backfill.py

--- backfill.py
+++ backfill.py
@@ -11,7 +11,7 @@ import pandas as pd
 import requests
 
 # 搜尋關鍵字與設定
-SEARCH_KEYWORDS = ["Microbiom", "metagenome", "metagenomic"]
+SEARCH_KEYWORDS = ["Microbio", "metagenome", "metagenomic"]
 SEARCH_TERM = " OR ".join(SEARCH_KEYWORDS)
 
 EXCEL_IF_PATH = "JCR-ImapctFactor-2025.xlsx"
